a3c61e4f05062fe16c3a78c59e964991d1a6c713
Daniel Seither <post@tiwoc.de>
2014-06-20 11:46:50 +0200

    Extract row.h/row.cpp

--- include/sqlitewrapper/statement.h
+++ include/sqlitewrapper/statement.h
@@ -8,65 +8,10 @@
 
 #include "exceptions.h"
 #include "nullable.h"
+#include "row.h"
 #include "util.h"
 
-struct sqlite3_stmt;
-
 namespace SqliteWrapper {
-
-class RowIterator;
-
-class Row
-{
-public:
-    Row(sqlite3_stmt *stmt);
-
-    bool isNull(int pos) const;
-
-    template <typename T>
-    T get(int pos) const
-    {
-        checkPosRange(pos);
-        return getUnchecked<T>(pos);
-    }
-
-    template <typename T>
-    Nullable<T> getNullable(int pos) const
-    {
-        checkPosRange(pos);
-        if (isNull(pos)) return Nullable<T>();
-        return Nullable<T>(getUnchecked<T>(pos));
-    }
-
-    // specialize this to add bindings for custom types
-    template <typename T>
-    T getUnchecked(int pos) const;
-
-private:
-    void setColumns(int columns);
-    void checkPosRange(int pos) const;
-
-    sqlite3_stmt *m_stmt = nullptr;
-    int m_columns = 0;
-
-    friend class RowIterator;
-};
-
-class RowIterator : public std::iterator<std::input_iterator_tag, Row, void>
-{
-public:
-    RowIterator(sqlite3_stmt *stmt, bool done);
-    bool operator==(const RowIterator &rhs) const;
-    bool operator!=(const RowIterator &rhs) const;
-    RowIterator &operator++();
-    Row &operator*();
-    Row *operator->();
-
-private:
-    sqlite3_stmt *m_stmt;
-    bool m_done;
-    Row m_row;
-};
 
 class Statement;
 
