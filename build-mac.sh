02fa15d554b52c10da320413b44291be04c08f64
Daniel Seither <post@tiwoc.de>
2014-07-04 18:00:26 +0200

    Add build scripts

---
+++ build-mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e  # exit immediately on error
+set -v  # verbose, echo all commands
+
+SOURCE_DIR=$(pwd)
+BUILD_DIR=build-sqlitewrapper
+CMAKE=cmake
+MAKE=make
+
+export PATH="/usr/local/bin:$PATH"
+
+cd ..
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+"$CMAKE" "$SOURCE_DIR"
+"$MAKE"
---
+++ build-windows.cmd
@@ -0,0 +1,17 @@
+set SOURCE_DIR=%CD%
+set BUILD_DIR=build-sqlitewrapper
+set CMAKE=cmake.exe
+set MAKE=mingw32-make.exe
+
+cd ..
+if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%" || goto error
+"%CMAKE%"  -G "MinGW Makefiles" "%SOURCE_DIR%" || goto error
+"%MAKE%" || goto error
+
+cd "%SOURCE_DIR%"
+exit /b 0
+
+:error
+cd "%SOURCE_DIR%"
+exit /b 1
