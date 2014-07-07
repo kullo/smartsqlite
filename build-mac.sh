9d2a1b30d2d24541fc0620006c02f2196951cd38
Daniel Seither <post@tiwoc.de>
2014-07-07 15:21:05 +0200

    Fix build scripts

--- build-mac.sh
+++ build-mac.sh
@@ -4,13 +4,13 @@ set -v  # verbose, echo all commands
 
 SOURCE_DIR=$(pwd)
 BUILD_DIR=build-sqlitewrapper
-CMAKE=cmake
-MAKE=make -j2
+CMAKE_COMMAND=cmake
+MAKE_COMMAND="make -j2"
 
 export PATH="/usr/local/bin:$PATH"
 
 cd ..
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
-$CMAKE "$SOURCE_DIR"
-$MAKE
+$CMAKE_COMMAND "$SOURCE_DIR"
+$MAKE_COMMAND
--- build-windows.cmd
+++ build-windows.cmd
@@ -1,13 +1,13 @@
 set SOURCE_DIR=%CD%
 set BUILD_DIR=build-sqlitewrapper
-set CMAKE=cmake.exe
-set MAKE=mingw32-make.exe -j2
+set CMAKE_COMMAND=cmake.exe
+set MAKE_COMMAND=mingw32-make.exe -j2
 
 cd ..
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 cd "%BUILD_DIR%" || goto error
-%CMAKE%  -G "MinGW Makefiles" "%SOURCE_DIR%" || goto error
-%MAKE% || goto error
+%CMAKE_COMMAND% -G "MinGW Makefiles" "%SOURCE_DIR%" || goto error
+%MAKE_COMMAND% || goto error
 
 cd "%SOURCE_DIR%"
 exit /b 0
