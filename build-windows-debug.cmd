a7ebddb39252706c0c47c87802ec65848bd78cdb
Simon Warta <simon@warta.it>
2015-07-06 20:24:39 +0200

    Take jom from PATH

--- build-windows-debug.cmd
+++ build-windows-debug.cmd
@@ -4,7 +4,7 @@ set CMAKE_BUILD_TYPE=Debug
 for /f %%c in ('wmic cpu get NumberOfCores ^| findstr [0-9]') do set "CORES=%%c"
 set SOURCE_DIR=%CD%
 set CMAKE_COMMAND=cmake.exe
-set MAKE_COMMAND=C:\Qt-Installation\Tools\QtCreator\bin\jom.exe /J %CORES%
+set MAKE_COMMAND=jom.exe /J %CORES%
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 
