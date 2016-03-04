b94f927841b0a40b987dea6789c80cc78d8b44ba
Simon Warta <simon@kullo.net>
2016-03-04 16:22:03 +0100

    Add updating tool for sqlite

---
+++ tools/update_sqlite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+which shellcheck > /dev/null && shellcheck "$0"
+
+
+VERSION="$1"
+
+FILENAME="sqlite-amalgamation-$VERSION"
+
+(
+    cd tmp
+    wget --output-document="$FILENAME.zip" \
+      "https://www.sqlite.org/2016/sqlite-amalgamation-$VERSION.zip"
+    sha1sum "$FILENAME.zip"
+    unzip -o "$FILENAME.zip"
+)
+
+cp "./tmp/$FILENAME/sqlite3.c" ../src/
+cp "./tmp/$FILENAME/sqlite3.h" ../include/smartsqlite
