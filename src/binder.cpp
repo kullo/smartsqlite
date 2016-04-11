602f5fbcc39bfe10bd517a50dbc723c9e0cc329d
Daniel Seither <post@tiwoc.de>
2016-04-11 17:46:46 +0200

    NativeBinder: Correctly bind empty strings and blobs
    Calling .c_str() on an empty string or .data() on an empty vector is
    allowed to return a nullptr. SQLite interprets passing a nullptr to
    sqlite3_bind_text or sqlite3_bind_blob as if sqlite3_bind_null had been
    called. So while we wanted to bind an empty string to the statement, we
    bind NULL, which is not the same thing ("" != NULL).
    
    This misbehavior is fixed by replacing an incoming nullptr with a non-
    null pointer if the size of the incoming data is zero.

--- src/binder.cpp
+++ src/binder.cpp
@@ -19,6 +19,12 @@
 
 namespace SmartSqlite {
 
+namespace {
+
+const char *EMPTY_CSTRING = "";
+
+}
+
 int NativeBinder::bindLongLong(sqlite3_stmt *stmt, int pos, long long value)
 {
     return sqlite3_bind_int64(stmt, pos + 1, value);
@@ -31,11 +37,15 @@ int NativeBinder::bindDouble(sqlite3_stmt *stmt, int pos, double value)
 
 int NativeBinder::bindString(sqlite3_stmt *stmt, int pos, const std::string &value)
 {
-    return sqlite3_bind_text(stmt, pos + 1, value.c_str(), value.size(), SQLITE_TRANSIENT);
+    auto data = value.c_str();
+    auto size = value.size();
+    if (!data && !size) data = EMPTY_CSTRING;
+    return sqlite3_bind_text(stmt, pos + 1, data, size, SQLITE_TRANSIENT);
 }
 
 int NativeBinder::bindBlob(sqlite3_stmt *stmt, int pos, const void *data, size_t size)
 {
+    if (!data && !size) data = EMPTY_CSTRING;
     return sqlite3_bind_blob(stmt, pos + 1, data, size, SQLITE_TRANSIENT);
 }
 
