73019124ff3e917218ddbe67462631599b297cf4
Simon Warta <simon@kullo.net>
2016-06-04 15:46:33 +0200

    Set version: v11

--- src/version.cpp
+++ src/version.cpp
@@ -23,7 +23,7 @@
 namespace SmartSqlite {
 
 namespace {
-const auto SMARTSQLITE_VERSION_CODE = int{10};
+const auto SMARTSQLITE_VERSION_CODE = int{11};
 const auto SMARTSQLITE_VERSION_STRING = "v" + std::to_string(SMARTSQLITE_VERSION_CODE);
 }
 
