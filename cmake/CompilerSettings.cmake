b5d86602e2e5a0c4f354197a0a34d55cb1c88d59
Daniel Seither <post@tiwoc.de>
2016-07-04 16:56:57 +0200

    Update hardening flags

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -11,13 +11,6 @@ elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99 -pthread ${warn_flags}")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pthread ${warn_flags} -Woverloaded-virtual")
 
-    # Hardening
-    set(hardening_flags "-fstack-protector -pie -fPIE -D_FORTIFY_SOURCE=2 -Wl,-z,relro,-z,now")
-    set(CMAKE_C_FLAGS_RELEASE          "${CMAKE_C_FLAGS_RELEASE}          ${hardening_flags}")
-    set(CMAKE_C_FLAGS_RELWITHDEBINFO   "${CMAKE_C_FLAGS_RELWITHDEBINFO}   ${hardening_flags}")
-    set(CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_RELEASE}        ${hardening_flags}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${hardening_flags}")
-
 # Workaround for CMake < 3.1: Variables such as 'MSVC' are expanded in 'if'
 # statements even if they are quoted. We can't enable the policy CMP0054 because
 # we need to support CMake 2.8. Therefore, we append a space on both sides,
@@ -32,4 +25,20 @@ if(APPLE)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.9")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.9")
     endif()
+
+# non-Apple unices
+elseif(UNIX)
+    # Hardening
+    set(hardening_cflags  "-fstack-protector -fPIC -D_FORTIFY_SOURCE=2")
+    set(hardening_ldflags "-Wl,-z,relro,-z,now")
+
+    set(CMAKE_C_FLAGS_RELEASE          "${CMAKE_C_FLAGS_RELEASE}          ${hardening_cflags}")
+    set(CMAKE_C_FLAGS_RELWITHDEBINFO   "${CMAKE_C_FLAGS_RELWITHDEBINFO}   ${hardening_cflags}")
+    set(CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_RELEASE}        ${hardening_cflags}")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${hardening_cflags}")
+
+    set(CMAKE_EXE_LINKER_FLAGS_RELEASE           "${CMAKE_EXE_LINKER_FLAGS_RELEASE}           -pie ${hardening_ldflags}")
+    set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO    "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO}    -pie ${hardening_ldflags}")
+    set(CMAKE_SHARED_LINKER_FLAGS_RELEASE        "${CMAKE_SHARED_LINKER_FLAGS_RELEASE}        -pic ${hardening_ldflags}")
+    set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO "${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO} -pic ${hardening_ldflags}")
 endif()
