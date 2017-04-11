c9c6fb5f0c09fcc0a16b3ff4209a4e56e666b8d4
Simon Warta <simon@kullo.net>
2017-04-11 08:53:01 +0200

    Update copyright year to 2017

--- test/nullable_test.cpp
+++ test/nullable_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014–2016 The SmartSqlite contributors (see NOTICE.txt)
+ * Copyright 2014–2017 The SmartSqlite contributors (see NOTICE.txt)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
