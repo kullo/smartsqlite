02cb055f005c2aec79bd5d23bc899ac92be33c6d
Simon Warta <simon@kullo.net>
2017-06-28 22:12:13 +0200

    Ignore warnings in sqlite3.c on older clang versions
    when building botansqlite3.c

--- src/botansqlite3/botansqlite3.c
+++ src/botansqlite3/botansqlite3.c
@@ -6,6 +6,23 @@
 
 #if defined __clang__
     #pragma clang diagnostic push
+
+    // ignore explicitly because clang 3.7 does not support
+    // "-Weverything" here.
+    #pragma clang diagnostic ignored "-Wcast-align"
+    #pragma clang diagnostic ignored "-Wcast-qual"
+    #pragma clang diagnostic ignored "-Wconditional-uninitialized"
+    #pragma clang diagnostic ignored "-Wconversion"
+    #pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
+    #pragma clang diagnostic ignored "-Wfloat-equal"
+    #pragma clang diagnostic ignored "-Wmissing-variable-declarations"
+    #pragma clang diagnostic ignored "-Wsign-compare"
+    #pragma clang diagnostic ignored "-Wsign-conversion"
+    #pragma clang diagnostic ignored "-Wundef"
+    #pragma clang diagnostic ignored "-Wunknown-pragmas"
+    #pragma clang diagnostic ignored "-Wunused-function"
+
+    // sufficient for newer clang versions
     #pragma clang diagnostic ignored "-Weverything"
 #endif
 
